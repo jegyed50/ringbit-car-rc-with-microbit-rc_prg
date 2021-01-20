a229056b68b3cc6c4422ffbeb220823d6bcefdd9
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-01-20 17:13:33 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ radio.setGroup(90)
 basic.forever(function () {
     radio.sendValue("x", Math.idiv(input.acceleration(Dimension.X), 10))
     basic.pause(100)
-    radio.sendValue("y", Math.idiv(input.acceleration(Dimension.Y), 10))
+    radio.sendValue("y", Math.idiv(input.acceleration(Dimension.Y), 10) * -1)
     basic.pause(100)
 })
