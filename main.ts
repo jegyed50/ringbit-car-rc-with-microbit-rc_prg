c2bee535fdf5eaf80d1a24aa15ebea779381f732
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-01-20 16:57:04 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,7 @@
+radio.setGroup(90)
+basic.forever(function () {
+    radio.sendValue("x", Math.idiv(input.acceleration(Dimension.X), 10))
+    basic.pause(100)
+    radio.sendValue("y", Math.idiv(input.acceleration(Dimension.Y), 10))
+    basic.pause(100)
+})
